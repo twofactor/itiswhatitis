0ea275551f29042514aae23a154b12a281d98f8f
jtannady <jtannady@users.noreply.github.com>
2020-06-26 23:49:47 -0400

    Update note.js

--- pages/note.js
+++ pages/note.js
@@ -45,7 +45,22 @@ export default function Home() {
             <p><Heading size="2xl">👁👄👁</Heading></p>
             <p><Heading size="2xl">WHAT IT REALLY IS!</Heading></p>
             <p>JUNE 26TH, 2020</p>
-            
+            <p>We’ve done pretty well for a non-existent product. 👁👄👁.fm was the top product of the day on Product
+            Hunt (Theranos who?); the website accumulated 20,000 email signups, thousands of tweets sharing the link,
+              coverage in <a
+                href="https://www.independent.co.uk/life-style/gadgets-and-tech/news/face-emoji-twitter-it-is-what-it-is-promo-a9587351.html">The
+                Independent</a>, <a
+                href="https://www.forbes.com/sites/paularmstrongtech/2020/06/26/what-is--oh-it-is-what-it-is/amp/?__twitter_impression=true">Forbes</a>,
+              we got shoutouts from <a href="https://constine.substack.com/p/what-does-mean-well">Josh Constine</a>, <a
+                href="https://wfh.substack.com/p/the-6-builders-who-will-thrive-in">Brianne Kimmel</a>, <a
+                  href="https://www.reddit.com/r/OutOfTheLoop/comments/hg26ip/whats_the_deal_with_this_it_is_what_it_is_app/">some
+                folks on Reddit puzzled over who we were</a>, <a
+                href="https://twitter.com/andrewchen/status/1276585276626726913?s=20">Andrew Chen</a>, <a
+                  href="https://twitter.com/shannonpurser/status/1276631647157235712">Shannon Purser of Stranger
+                Things</a>, and <a href="https://twitter.com/elonmusk/status/1276418907968925696">Elon Musk may have
+                subtweeted us</a>? The <a href="https://twitter.com/itiseyemoutheye">@itiseyemoutheye</a> Twitter and
+              accounts of our teammates were inundated with invite requests. Most importantly, we raised over $60,000 in
+              donations from people who hoped to get special treatment within our fabled waitlist.</p>
             <p>In a strange way, this sort of became an anti-statement against what we’d all seen on tech Twitter. We’re
               a <a href="/demographics.png">diverse</a>, ragtag group of young technologists tired of the
               status quo tech industry, and thought that we could make the industry think a bit more about its actions.
