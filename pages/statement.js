281df7f0c228b4f190fc7504ff950fcfe3a4ed93
David <davidbui.psd@gmail.com>
2020-06-26 20:23:21 -0700

    master?

--- pages/statement.js
+++ pages/statement.js
@@ -47,29 +47,29 @@ export default function Home() {
             <p>JUNE 26TH, 2020</p>
             <p>You’re probably wondering what this is. Well, it started off as just what it is.</p>
             <p>A group of us changed our Twitter names to include "👁👄👁" because we thought it was a funny trend from
-              TikTok. People started noticing the change on their timelines, noting the creepiness of the emojis in
-              particular. For a brief period of time, everyone who added the emojis to their name were added to a giant
+            TikTok. People started noticing the change on their timelines, noting the creepiness of the emojis in
+            particular. For a brief period of time, everyone who added the emojis to their name were added to a giant
               Twitter group conversation. From there, things unfolded.</p>
             <p>What started out as a meme in our small group chat grew bigger than we ever imagined. So we thought about
-              how to make use of the hype cycle we’d stumbled upon. But honestly, we didn’t have to think too hard: in
-              this moment, there’s pretty much no greater issue to amplify than the systemic racism and anti-Blackness
-              much of the world is only beginning to wake up to. We’re excited that we could use our newfound platform
+            how to make use of the hype cycle we’d stumbled upon. But honestly, we didn’t have to think too hard: in
+            this moment, there’s pretty much no greater issue to amplify than the systemic racism and anti-Blackness
+            much of the world is only beginning to wake up to. We’re excited that we could use our newfound platform
               to drive action towards a few causes that are doing important work towards racial justice: <a
                 href="https://thelovelandfoundation.org/loveland-therapy-fund/">Loveland Foundation Therapy Fund</a>, <a
-                href="https://www.theokraproject.com/">The Okra Project</a>, <a
-                href="https://www.innocenceproject.org/">The Innocence Project</a>, and others.</p>
+                  href="https://www.theokraproject.com/">The Okra Project</a>, <a
+                    href="https://www.innocenceproject.org/">The Innocence Project</a>, and others.</p>
             <p>We’ve done pretty well for a non-existent product. 👁👄👁.fm was the top product of the day on Product
-              Hunt (Theranos who?); the website accumulated 20,000 email signups, thousands of tweets sharing the link,
+            Hunt (Theranos who?); the website accumulated 20,000 email signups, thousands of tweets sharing the link,
               coverage in <a
                 href="https://www.independent.co.uk/life-style/gadgets-and-tech/news/face-emoji-twitter-it-is-what-it-is-promo-a9587351.html">The
                 Independent</a>, <a
                 href="https://www.forbes.com/sites/paularmstrongtech/2020/06/26/what-is--oh-it-is-what-it-is/amp/?__twitter_impression=true">Forbes</a>,
               we got shoutouts from <a href="https://constine.substack.com/p/what-does-mean-well">Josh Constine</a>, <a
                 href="https://wfh.substack.com/p/the-6-builders-who-will-thrive-in">Brianne Kimmel</a>, <a
-                href="https://www.reddit.com/r/OutOfTheLoop/comments/hg26ip/whats_the_deal_with_this_it_is_what_it_is_app/">some
+                  href="https://www.reddit.com/r/OutOfTheLoop/comments/hg26ip/whats_the_deal_with_this_it_is_what_it_is_app/">some
                 folks on Reddit puzzled over who we were</a>, <a
                 href="https://twitter.com/andrewchen/status/1276585276626726913?s=20">Andrew Chen</a>, <a
-                href="https://twitter.com/shannonpurser/status/1276631647157235712">Shannon Purser of Stranger
+                  href="https://twitter.com/shannonpurser/status/1276631647157235712">Shannon Purser of Stranger
                 Things</a>, and <a href="https://twitter.com/elonmusk/status/1276418907968925696">Elon Musk may have
                 subtweeted us</a>? The <a href="https://twitter.com/itiseyemoutheye">@itiseyemoutheye</a> Twitter and
               accounts of our teammates were inundated with invite requests. Most importantly, we raised over $60,000 in
@@ -83,11 +83,24 @@ export default function Home() {
               people all over the world, and exclude these folks from the building process. As an industry, we need to
               do better.</p>
             <p>We sincerely thank you for spreading the word and donating to these important causes. In conclusion, it
-              is what it is: a meme that leveraged the relentless hype of exclusive apps and redirected it towards a
-              critical social need. Thank you, and remember that unlike 👁👄👁, #BlackLivesMatter and other social
+            is what it is: a meme that leveraged the relentless hype of exclusive apps and redirected it towards a
+            critical social need. Thank you, and remember that unlike 👁👄👁, #BlackLivesMatter and other social
               movements aren't trends or hype cycles. Let’s keep giving back as best as we can.</p>
             <p>Signed,</p>
             <p><a href="https://twitter.com/itiseyemoutheye/following">The 👁👄👁 Team</a></p>
+            <br />
+
+            <p>
+              <a href="https://shop.itiswhatitis.fm/">
+                We do have merch though.
+                </a>
+            </p>
+            <p>
+              ‎‎‏‏‎Use the password{" "}
+              <strong className="password">BlackLivesMATTER</strong>
+            </p>
+            <br />
+
             <p><strong>P.S.: Some of us are job hunting. Hire us!</strong></p>
             <ul>
               <li><a href="http://dtbui.com/">David</a> is looking for software engineering and design roles.</li>
@@ -103,7 +116,7 @@ export default function Home() {
             </ul>
           </article>
         </Box>
-        <img src={"https://a.intern.world/statement.png"}/>
+        <img src={"https://a.intern.world/statement.png"} />
       </div>
       <style global jsx>{`
       article {
